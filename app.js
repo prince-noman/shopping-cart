87f2e96dac7a0fb6b21cf66295938758e6f40a6d
prince-noman <prince1000bd@gmail.com>
2021-09-30 04:50:34 +0600

    final project uploaded

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function calculateTotal() {
 	//update on the html
 	document.getElementById("sub-total").innerText = subTotal;
 	document.getElementById("tax-amount").innerText = tax;
-	document.getElementById("total-price").innerText = totalPrice;
+	document.getElementById("total-price").innerText = totalPricegit;
 }
 
 //Handle Phone increase decrease event
